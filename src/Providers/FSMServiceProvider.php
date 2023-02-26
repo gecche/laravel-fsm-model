65b8d851ef93a529d785e8c34af0a806cb2d4e59
Giacomo Terreni <giacomoterreni@gmail.com>
2023-02-26 17:33:26 +0100

    first beta (2)

--- src/Providers/FSMServiceProvider.php
+++ src/Providers/FSMServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Gecche\FSM\Providers;
 
+use Gecche\FSM\Commands\FSMMigrationCommand;
 use Illuminate\Support\ServiceProvider;
 use Gecche\FSM\FSMManager;
 
@@ -32,11 +33,20 @@ class FSMServiceProvider extends ServiceProvider
     public function boot()
     {
 
+        $this->registerCommands();
         $this->publishes([
             __DIR__.'/config/fsm.php' => config_path('fsm.php'),
             __DIR__.'/App/Listeners/HandleStatusTransition.php' => app_path('Listeners/HandleStatusTransition.php'),
         ]);
 
+    }
+
+    public function registerCommands() {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                FSMMigrationCommand::class,
+            ]);
+        }
     }
 
 }
